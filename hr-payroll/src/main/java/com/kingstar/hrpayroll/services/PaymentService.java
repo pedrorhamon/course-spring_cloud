e563dda0100397d9a80678ef74fac34e5880761c
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-05 22:07:59 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/kingstar/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.kingstar.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.kingstar.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
